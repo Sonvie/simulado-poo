5e35691dafea9dce14af03379df5d06770f64516
Sonvie <jefersonjjxz48@gmail.com>
2026-09-24 21:23:54 -0300

    questao-04

---
+++ GestaoPedidos.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestaoPedidos {
+    private List<String> pedidos;
+
+    public GestaoPedidos(){
+        this.pedidos = new ArrayList<>();
+    }
+
+    public void adicionarPedido(String item){
+        pedidos.add(item);
+    }
+
+    public String proximoPedido(){
+        if (pedidos.get(0) == null){
+            return "Fila Vazia.";
+        }
+        String pedido = pedidos.get(0);
+        pedidos.remove(0);
+        return pedido;
+    }
+
+    public int quantidadePendentes(){
+        return pedidos.size();
+    }
+
+    public void listarPedidos(){
+        for (int i = 0; i < pedidos.size(); i++){
+            System.out.println(pedidos.get(i));
+        }
+    }
+
+    public static void main(String args[]){
+        GestaoPedidos pedidos = new GestaoPedidos();
+        pedidos.adicionarPedido("Notebook");
+        pedidos.adicionarPedido("Celular");
+        pedidos.adicionarPedido("Carregador");
+
+        pedidos.proximoPedido();
+
+        System.out.println(pedidos.quantidadePendentes());
+    }
+
+}
+
